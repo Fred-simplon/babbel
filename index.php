4348193a46b7944e6d1829daa7eb2531452f8cd9
Fred-simplon <rousselin.frederic12@gmail.com>
2018-12-18 17:05:06 +0100

    second push

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>A Simplon, on approuve Babbel.</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header>
+		<div>
+			<img src="https://www.babbel.com/static/index_page/en-US/img/logo.svg" class="logo">
+			<nav class="stroke">
+				<ul>
+					<li><a href="#discover">Découvrir</a></li>
+					<li><a href="#download">Télécharger</a></li>
+					<li><a href="#contact">Contactez-nous</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<main>
+		<!--Presentation -->
+		<div class="presentation">
+			<h1>Parlez une langue comme vous en avez toujours rêvé</h1>
+			<div class="button_dl" align="center">
+				<a class="DownloadBtn" href="#Download">Télécharger</a>
+			</div>
+
+		</div>
+		<!--Discover -->
+		<div id="discover">
+			<h2>Avec Babbel, découvrez un nouveau monde</h2>
+			<div class="flex-container-cat">
+				<div class="flex-box-cat">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquid deserunt excepturi eum laborum explicabo. Sint eum facere officia maiores consequatur quasi repudiandae consectetur, similique, at unde quod quo quis?</p>
+				</div>
+				<div class="flex-box-cat">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquid deserunt excepturi eum laborum explicabo. Sint eum facere officia maiores consequatur quasi repudiandae consectetur, similique, at unde quod quo quis?</p>
+				</div>
+				<div class="flex-box-cat">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquid deserunt excepturi eum laborum explicabo. Sint eum facere officia maiores consequatur quasi repudiandae consectetur, similique, at unde quod quo quis?</p>
+				</div>
+			</div>
+
+		</div>
+		<!--Download -->
+		<div id="Download">
+
+		</div>
+		<!--Contact -->
+		<div>
+
+		</div>
+
+
+	</main>
+
+	<footer>
+		<div>
+			<ul>
+				<li>Babbel.fr</li>
+				<li>Mentions légales</li>
+				<ul>
+					<li>Téléchargements</li>
+					<li>Android</li>
+					<li>IoS</li>
+				</ul>
+			</ul>
+
+		</div>
+		<div>
+			<form action="newsletter.php" method="POST">
+				<div>
+					<label>Newsletter</label>
+					<input type="mail" name="mailNews">
+					<button type="submit">Envoyer</button>
+				</div>
+			</form>
+
+		</div>
+		<p></p>
+
+	</footer>
+
+</body>
+</html>
